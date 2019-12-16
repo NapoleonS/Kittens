54c65a4da773ca2199c8769850ef2c61515e0928
Napoleon <napoleon.sanmartin@gmail.com>
2019-12-16 12:50:40 -0300

    Updates

---
+++ precache-manifest.1e2cb7884b7ddf9a6c6db1042def9de6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817820af69ca83290707132df2683117",
+    "url": "/Kittens/index.html"
+  },
+  {
+    "revision": "3c829766f61b7a885853",
+    "url": "/Kittens/static/css/main.c2e26455.chunk.css"
+  },
+  {
+    "revision": "d2375efbcf7256d071bb",
+    "url": "/Kittens/static/js/2.cf8a7680.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/Kittens/static/js/2.cf8a7680.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c829766f61b7a885853",
+    "url": "/Kittens/static/js/main.63421c14.chunk.js"
+  },
+  {
+    "revision": "f5cff1ff45568026a7cc",
+    "url": "/Kittens/static/js/runtime-main.d32ec3a1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Kittens/precache-manifest.1e2cb7884b7ddf9a6c6db1042def9de6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Kittens/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
